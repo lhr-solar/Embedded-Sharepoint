ea29f470d898a5e9a6c9978cd0b9010c269713f9
KadhirV <kadhir.vivek@gmail.com>
2024-09-22 12:38:11 -0500

    Can Test

--- Test/Tests/blinky.c
+++ Test/Tests/blinky.c
@@ -1,20 +1,32 @@
+#include <stdint.h>
 #include <stm32xx_hal.h>
 
-int main(){
+int main() {
     HAL_Init();
 
+    // Configure PB4 as an output pin
     GPIO_InitTypeDef led_config = {
         .Mode = GPIO_MODE_OUTPUT_PP,
         .Pull = GPIO_NOPULL,
-        .Pin = GPIO_PIN_5
+        .Pin = GPIO_PIN_4  // Configure PB4
+    };
+
+    GPIO_InitTypeDef led_config1 = {
+        .Mode = GPIO_MODE_OUTPUT_PP,
+        .Pull = GPIO_NOPULL,
+        .Pin = GPIO_PIN_5  // Configure PB4
     };
     
-    __HAL_RCC_GPIOA_CLK_ENABLE(); // enable clock for GPIOA
-    HAL_GPIO_Init(GPIOA, &led_config); // initialize GPIOA with led_config
+    __HAL_RCC_GPIOB_CLK_ENABLE(); // Enable clock for GPIOB
+    HAL_GPIO_Init(GPIOB, &led_config); // Initialize GPIOB with led_config
+    HAL_GPIO_Init(GPIOB, &led_config1);
 
-    while(1){
-        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+    // Main loop
+    while (1) {
+        // Turn PB4 ON
+        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_5);
         HAL_Delay(500);
+       
     }
 
     return 0;
