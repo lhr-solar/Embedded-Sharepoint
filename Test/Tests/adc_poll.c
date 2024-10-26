cf7313b353b3e01a2bcd892d4c0b8f49eaac2c91
Aarav Mahesh <aarav.mahesh@gmail.com>
2024-10-26 02:04:37 -0500

    code before using auto-generated cube

--- Test/Tests/adc_poll.c
+++ Test/Tests/adc_poll.c
@@ -22,15 +22,31 @@ int main() {
     #endif
 
     ADC_HandleTypeDef adc;
-    HAL_ADC_Init(&adc);
     
+    // configure adc
+    adc.Instance = ADC1;
+    adc.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+    adc.Init.Resolution = ADC_RESOLUTION_12B;
+    adc.Init.ScanConvMode = DISABLE;
+    adc.Init.ContinuousConvMode = DISABLE;
+    adc.Init.DiscontinuousConvMode = DISABLE;
+    adc.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    adc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    adc.Init.NbrOfConversion = 1;
+
+    volatile HAL_StatusTypeDef status = HAL_ADC_Init(&adc);
+    status += 0;
+
+    if (HAL_ADC_Init(&adc) != HAL_OK) {
+        return HAL_ERROR;
+    }
 
     // Test A --- Poll for ADC conversion
     volatile uint32_t adc_val; // turn off compiler optimization to view variable
     HAL_ADC_Start(&adc);
 
     while(1) {
-        HAL_ADC_PollForConversion(&adc, 100);
+        HAL_ADC_PollForConversion(&adc, 10000); // use max delay
         adc_val = HAL_ADC_GetValue(&adc);
         //printf(adc_val);
         adc_val += 0; // c needs some sort of change on adc [filler line]
