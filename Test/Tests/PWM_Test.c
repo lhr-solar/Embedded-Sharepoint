a601ccc68d8a03593ee7c3b802d2f73ae049cdd3
Phoenix Nguyen <phoenixn831@gmail.com>
2024-11-09 13:54:41 -0600

    PWM_Test

---
+++ Test/Tests/PWM_Test.c
@@ -0,0 +1,62 @@
+#include <stdint.h>
+#include "stm32xx_hal.h"
+
+int main(){
+
+    HAL_Init();
+
+    GPIO_InitTypeDef pwm_config = {
+        .Mode = GPIO_MODE_AF_PP,
+        .Pull = GPIO_NOPULL,
+        .Pin = GPIO_PIN_8,
+        .Speed = GPIO_SPEED_FREQ_LOW,
+        .Alternate = GPIO_AF1_TIM1
+    };
+
+    // GPIO_InitTypeDef led_config = {
+    //     .Mode = GPIO_MODE_OUTPUT_PP,
+    //     .Pull = GPIO_NOPULL,
+    //     .Pin = GPIO_PIN_5,
+    //     .Speed = GPIO_SPEED_FREQ_LOW
+    // };
+
+    
+    __HAL_RCC_GPIOA_CLK_ENABLE(); // enable clock for GPIOA
+    HAL_GPIO_Init(GPIOA, &pwm_config); // initialize GPIOA with pwm_config
+    // HAL_GPIO_Init(GPIOA, &led_config);
+
+    TIM_HandleTypeDef htim1;
+    htim1.Instance = TIM1;
+    htim1.Init.Prescaler = 8 - 1;
+    htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
+    htim1.Init.Period = 10000 - 1;
+    // htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+    htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+
+    HAL_TIM_PWM_Init(&htim1);
+
+    TIM_OC_InitTypeDef sConfigOC = {0};
+    sConfigOC.OCMode = TIM_OCMODE_PWM1;  // Set PWM mode 1
+    sConfigOC.Pulse = 5000;               // Set the pulse to 50% of the period (500 out of 1000)
+    sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+    sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+
+    HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_1);
+
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+    // TIM1->CCR1 = 500;
+    // __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, 500);
+
+    while(1) {
+        // HAL_Delay(10000);
+        // HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+    }
+}
+
+// void HAL_TIM_PWM_IRQHandler(TIM_HandleTypeDef *htim) {
+//     HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+// }
+
+// void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef *htim) {
+//     HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
+// }
