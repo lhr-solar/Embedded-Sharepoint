c8544e5ffc04c014fb64cd7678a9e8868cc62485
Nathaniel Delgado <nathaniel.delgado@utexas.edu>
2024-11-09 15:30:24 -0600

    Looks good

--- driver/Inc/CAN.h
+++ driver/Inc/CAN.h
@@ -1,49 +1,22 @@
 #ifndef CAN_H
 #define CAN_H
 
-typedef enum CAN_Status {
-    OK = 0,
-    INIT,
-    CONFIG,
-    START,
-    SEMAPHORE_CREATE,
-    QUEUE_CREATE,
-    INVALID_INSTANCE,
-    NOT_INITIALIZED,
-    SEMAPHORE_TAKE,
-    ADD_MESSAGE,
-    QUEUE_SEND,
-    ACTIVATE_NOTIFICATION,
-    YESSS,
-    BAD,
-    ENTRY_NOT_FOUND
-} CAN_Status_e;
+// return code for can driver
+typedef enum {
+    CAN_ERR,
+    CAN_OK,
+    CAN_SENT,
+    CAN_RECV,
+    CAN_EMPTY,
+} can_status_t;
 
-#define CAN_DATA_SIZE (8)
+// can handlers
+#ifdef CAN1
+extern CAN_HandleTypeDef* hcan1;
+#endif /* CAN1 */
 
-typedef struct {
-    CAN_TxHeaderTypeDef header;
-    uint8_t data[CAN_DATA_SIZE];
-} payload_tx_t;
-
-typedef struct {
-    CAN_RxHeaderTypeDef header;
-    uint8_t data[CAN_DATA_SIZE];
-} payload_rx_t;
-
-typedef struct {
-    uint32_t ID;
-    uint32_t size;
-    QueueHandle_t queue;
-    uint8_t* queueStorage;
-    StaticQueue_t queueControlBlock;
-} can_recv_entry_t;
-
-
-extern CAN_HandleTypeDef* can1_handle;
-
-CAN_Status_e can_init(CAN_HandleTypeDef *handle, CAN_FilterTypeDef *filter);
-CAN_Status_e can_send(CAN_HandleTypeDef *handle, const CAN_TxHeaderTypeDef *header, const uint8_t data[], bool blocking);
-CAN_Status_e can_recv(CAN_HandleTypeDef *handle, uint32_t ID, payload_rx_t* payload, bool blocking);
+can_status_t can_init(CAN_HandleTypeDef* handle, CAN_FilterTypeDef* filter);
+can_status_t can_send(CAN_HandleTypeDef* handle, const CAN_TxHeaderTypeDef* header, const uint8_t data[], bool blocking);
+can_status_t can_recv(CAN_HandleTypeDef* handle, uint16_t id, CAN_RxHeaderTypeDef* header, uint8_t data[], bool blocking);
 
 #endif /* CAN_H */
--- driver/Src/CAN.c
+++ driver/Src/CAN.c
@@ -1,44 +1,80 @@
 #include "stm32xx_hal.h"
 #include "CAN.h"
 
+// 8 for now unless extended payload is supported
+#define DATA_SIZE (8)
+
+// entries in queues
+typedef struct {
+    CAN_TxHeaderTypeDef header;
+    uint8_t data[DATA_SIZE];
+} tx_payload_t;
+
+typedef struct {
+    CAN_RxHeaderTypeDef header;
+    uint8_t data[DATA_SIZE];
+} rx_payload_t;
+
+// metadata for recieve queues
+typedef struct {
+    uint16_t id;
+    uint16_t size;
+    QueueHandle_t queue;
+    uint8_t* storage;
+    StaticQueue_t buffer;
+} recv_entry_t;
+
+#ifdef CAN1
+// fallback send queue size
 #ifndef CAN1_SEND_QUEUE_SIZE
 #define CAN1_SEND_QUEUE_SIZE (10)
 #endif
 
-#ifdef CAN1
-static CAN_HandleTypeDef can1 = {.Instance = CAN1};
-CAN_HandleTypeDef* can1_handle = &can1;
+// can1 handle
+static CAN_HandleTypeDef hcan1_ = {.Instance = CAN1};
+CAN_HandleTypeDef* hcan1 = &hcan1_;
+
+// can1 send semaphore
 static SemaphoreHandle_t can1_send_semaphore = NULL;
 static StaticSemaphore_t can1_send_semaphore_buffer;
+
+// can1 send queue
 static QueueHandle_t can1_send_queue = NULL;
 static StaticQueue_t can1_send_queue_buffer;
-static uint8_t can1_send_queue_storage[CAN1_SEND_QUEUE_SIZE * sizeof(payload_tx_t)];
+static uint8_t can1_send_queue_storage[CAN1_SEND_QUEUE_SIZE * sizeof(tx_payload_t)];
 
+// can1 recv semaphore
 static SemaphoreHandle_t can1_recv_semaphore = NULL;
 static StaticSemaphore_t can1_recv_semaphore_buffer;
 
-#define CAN_RECV_ENTRY(_ID, _SIZE) \
-    static uint8_t queueStorage_##_ID[_SIZE * sizeof(payload_rx_t)]; \
+// create recv queue storage
+#define CAN_RECV_ENTRY(ID_, SIZE_) \
+    static uint8_t recv_queue_storage_##ID_[SIZE_ * sizeof(rx_payload_t)]; \
 
 #include "can_entries.h"
 
 #undef CAN_RECV_ENTRY
-#define CAN_RECV_ENTRY(_ID, _SIZE) \
+
+// create recv queue array
+#define CAN_RECV_ENTRY(ID_, SIZE_) \
     { \
-        .ID = (_ID), \
-        .size = (_SIZE), \
+        .id = (ID_), \
+        .size = (SIZE_), \
         .queue = NULL, \
-        .queueStorage = queueStorage_##_ID, \
-        .queueControlBlock = {{0}} \
+        .storage = recv_queue_storage_##ID_, \
+        .buffer = {{0}} \
     },
 
-can_recv_entry_t can1_recv_entries[] = {
+static recv_entry_t can1_recv_entries[] = {
     #include "can_entries.h"
 };
 #undef CAN_RECV_ENTRY
-const uint32_t can1_recv_entry_count = sizeof(can1_recv_entries) / sizeof(can1_recv_entries[0]);
+
+// calculate amount of can1 recv entries
+static const uint32_t can1_recv_entry_count = sizeof(can1_recv_entries) / sizeof(can1_recv_entries[0]);
 #endif
 
+// safety check send/recv
 static bool initialized = false;
 
 // CAN MSP init
@@ -95,67 +131,66 @@ void HAL_CAN_MspDeInit(CAN_HandleTypeDef *hcan) {
     }
 }
 
-CAN_Status_e can_init(CAN_HandleTypeDef *handle, CAN_FilterTypeDef *filter) {
-    // init semaphore and queues
+can_status_t can_init(CAN_HandleTypeDef* handle, CAN_FilterTypeDef* filter) {
     if (handle->Instance == CAN1) {
+        // init semaphores
         can1_send_semaphore = xSemaphoreCreateBinaryStatic(
             &can1_send_semaphore_buffer);
         xSemaphoreGive(can1_send_semaphore);
         can1_recv_semaphore = xSemaphoreCreateBinaryStatic(
             &can1_recv_semaphore_buffer);
         xSemaphoreGive(can1_recv_semaphore);
+
+        // init queues
         can1_send_queue = xQueueCreateStatic(
             CAN1_SEND_QUEUE_SIZE,
-            sizeof(payload_tx_t),
+            sizeof(tx_payload_t),
             can1_send_queue_storage,
             &can1_send_queue_buffer);
-        for (uint32_t i = 0; i < can1_recv_entry_count; i++) {
+        for (int i = 0; i < can1_recv_entry_count; i++) {
             can1_recv_entries[i].queue = xQueueCreateStatic(
                 can1_recv_entries[i].size,
-                sizeof(payload_rx_t),
-                can1_recv_entries[i].queueStorage,
-                &can1_recv_entries[i].queueControlBlock
+                sizeof(rx_payload_t),
+                can1_recv_entries[i].storage,
+                &can1_recv_entries[i].buffer
             );
         }
     } else {
-        return INVALID_INSTANCE;
+        return CAN_ERR;
     }
-
 
     // init HAL
     if (HAL_CAN_Init(handle) != HAL_OK) {
-        return INIT;
+        return CAN_ERR;
     }
 
     // init filter
     if (HAL_CAN_ConfigFilter(handle, filter) != HAL_OK) {
-        return CONFIG;
+        return CAN_ERR;
     }
 
     // enable interrupts
     if (HAL_CAN_ActivateNotification(handle, CAN_IT_TX_MAILBOX_EMPTY) != HAL_OK) {
-        return ACTIVATE_NOTIFICATION;
+        return CAN_ERR;
     }
     if (HAL_CAN_ActivateNotification(handle, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
     {
-        return ACTIVATE_NOTIFICATION;
+        return CAN_ERR;
     }
 
     // start CAN
     if (HAL_CAN_Start(handle) != HAL_OK) {
-        return START;
+        return CAN_ERR;
     }
 
     initialized = true;
-    return OK;
+    return CAN_OK;
 }
 
-CAN_Status_e can_recv(CAN_HandleTypeDef *handle, uint32_t ID, payload_rx_t* payload, bool blocking) {
+can_status_t can_recv(CAN_HandleTypeDef* handle, uint16_t id, CAN_RxHeaderTypeDef* header, uint8_t data[], bool blocking) {
     if (!initialized) {
-        return NOT_INITIALIZED;
+        return CAN_ERR;
     }
-
-    int ret = OK;
 
     // determine timeout
     TickType_t timeout = (blocking) ? portMAX_DELAY : 0;
@@ -163,48 +198,69 @@ CAN_Status_e can_recv(CAN_HandleTypeDef *handle, uint32_t ID, payload_rx_t* payl
     // get semaphore
     if (handle->Instance == CAN1) {
         if (xSemaphoreTake(can1_recv_semaphore, timeout) != pdTRUE) {
-            return SEMAPHORE_TAKE;
+            return CAN_ERR;
         }
     } else {
-        return INVALID_INSTANCE;
+        return CAN_ERR;
     }
-    
-    // disable interrupts
+
+    // disable interrupts (don't want to CAN_RX to interrupt here)
     portENTER_CRITICAL();
 
-    // something here
-    int i;
-    for (i = 0; i < can1_recv_entry_count; i++) {
-        if (can1_recv_entries[i].ID == ID) {
-            if (xQueueReceive(
-                can1_recv_entries[i].queue,
-                payload,
-                timeout
-            ) != pdTRUE) {
-                ret = BAD;
+    can_status_t status = CAN_RECV;
+
+    // recieve from queue matching id
+    rx_payload_t payload = {0};
+    bool valid_id = false;
+    for (int i = 0; i < can1_recv_entry_count; i++) {
+        if (can1_recv_entries[i].id == id) {
+            valid_id = true;
+            // if blocking, retry on empty
+            if (blocking) {
+                while (xQueueReceive(can1_recv_entries[i].queue, &payload, 0) == errQUEUE_EMPTY) {
+                    // quickly enable interrupts so maybe next time we can hit
+                    portEXIT_CRITICAL();
+                    portENTER_CRITICAL();
+                }
+            
+            // otherwise, fail on empty
+            } else {
+                if (xQueueReceive(
+                    can1_recv_entries[i].queue, &payload, 0) == errQUEUE_EMPTY) {
+                    status = CAN_EMPTY;
+                    goto failed;
+                }
             }
         }
     }
-    
+
+    // decode payload if if is valid and message recieved
+    if (valid_id) {
+        *header = payload.header;
+        for (int i = 0; i < DATA_SIZE; i++) {
+            data[i] = payload.data[i];
+        }
+    } else { 
+        status = CAN_ERR;
+        goto failed;
+    }
+
+failed:
     // give semaphore
     if (handle->Instance == CAN1) {
         xSemaphoreGive(can1_recv_semaphore);
-    } else {
-        ret = INVALID_INSTANCE;
     }
     
     // enable interrupts
     portEXIT_CRITICAL();
 
-    return ret;
+    return status;
 }
 
-CAN_Status_e can_send(CAN_HandleTypeDef *handle, const CAN_TxHeaderTypeDef *header, const uint8_t data[], bool blocking) {
+can_status_t can_send(CAN_HandleTypeDef* handle, const CAN_TxHeaderTypeDef* header, const uint8_t data[], bool blocking) {
     if (!initialized) {
-        return NOT_INITIALIZED;
+        return CAN_ERR;
     }
-
-    int ret = OK;
 
     // determine timeout
     TickType_t timeout = (blocking) ? portMAX_DELAY : 0;
@@ -212,69 +268,65 @@ CAN_Status_e can_send(CAN_HandleTypeDef *handle, const CAN_TxHeaderTypeDef *head
     // get semaphore
     if (handle->Instance == CAN1) {
         if (xSemaphoreTake(can1_send_semaphore, timeout) != pdTRUE) {
-            return SEMAPHORE_TAKE;
+            return CAN_ERR;
         }
     } else {
-        return INVALID_INSTANCE;
+        return CAN_ERR;
     }
     
-    // disable interrupts
+    // disable interrupts (don't want CAN_TX to interrupt here)
     portENTER_CRITICAL();
+
+    can_status_t status = CAN_SENT;
 
     // if transmit is inactive, put payload into mailbox
     if (HAL_CAN_GetTxMailboxesFreeLevel(handle) >= 1) {
         uint32_t mailbox;
         if (HAL_CAN_AddTxMessage(handle, header, data, &mailbox) != HAL_OK) {
-            ret = ADD_MESSAGE;
+            status = CAN_ERR;
             goto failed;
-        } else {
-            ret = YESSS;
         }
     }
-    // otherwise, put into queue
+
+    // otherwise, put into send queue
     else {
-        payload_tx_t payload = {0};
+        tx_payload_t payload = {0};
         payload.header = *header;
-        for (int i = 0; i < CAN_DATA_SIZE; i++) {
+        for (int i = 0; i < DATA_SIZE; i++) {
             payload.data[i] = data[i];
         }
         if (handle->Instance == CAN1) {
             if (xQueueSend(can1_send_queue, &payload, timeout) != pdTRUE) {
-                ret = QUEUE_SEND;
+                status = CAN_ERR;
                 goto failed;
             }
-        } else {
-            ret = INVALID_INSTANCE;
-            goto failed;
-        }   
+        }
     }
 
 failed:
     // give semaphore
     if (handle->Instance == CAN1) {
         xSemaphoreGive(can1_send_semaphore);
-    } else {
-        ret = INVALID_INSTANCE;
     }
     
     // enable interrupts
     portEXIT_CRITICAL();
 
-    return ret;
+    return status;
 }
 
-void transmit_function(CAN_HandleTypeDef *hcan) {
-    // create payload
-    payload_tx_t payload = {0};
-
-    // receive data from queue
+static void transmit(CAN_HandleTypeDef* handle) {
+    tx_payload_t payload = {0};
     BaseType_t higherPriorityTaskWoken = pdFALSE;
-    if (hcan->Instance == CAN1) {
+
+    // receive data from send queue
+    if (handle->Instance == CAN1) {
         if (xQueueReceiveFromISR(can1_send_queue, &payload, &higherPriorityTaskWoken) == pdTRUE) {
             // add payload to mailbox
             uint32_t mailbox;
-            if (HAL_CAN_AddTxMessage(hcan, &payload.header, payload.data, &mailbox) != HAL_OK) {
+            if (HAL_CAN_AddTxMessage(handle, &payload.header, payload.data, &mailbox) != HAL_OK) {
                 // Handle transmission error (optional: log or retry mechanism)
+                // treated as lost packet for now
             }
         }
     }
@@ -284,29 +336,30 @@ void transmit_function(CAN_HandleTypeDef *hcan) {
 
 void HAL_CAN_TxMailbox0CompleteCallback(CAN_HandleTypeDef *hcan) {
     if (hcan->Instance == CAN1) {
-        transmit_function(hcan);
+        transmit(hcan);
     }
 }
 
 void HAL_CAN_TxMailbox1CompleteCallback(CAN_HandleTypeDef *hcan) {
     if (hcan->Instance == CAN1) {
-        transmit_function(hcan);
+        transmit(hcan);
     }
 }
 
 void HAL_CAN_TxMailbox2CompleteCallback(CAN_HandleTypeDef *hcan) {
     if (hcan->Instance == CAN1) {
-        transmit_function(hcan);
+        transmit(hcan);
     }
 }
 
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
+    rx_payload_t payload = {0};
     BaseType_t higherPriorityTaskWoken = pdFALSE;
-    payload_rx_t payload = {0};
 
+    // recieve messages from queue till empty and put into recieve queues
     while (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &payload.header, payload.data) == HAL_OK) {
         for (int i = 0; i < can1_recv_entry_count; i++) {
-            if (can1_recv_entries[i].ID == payload.header.StdId) {
+            if (can1_recv_entries[i].id == payload.header.StdId) {
                 xQueueSendFromISR(can1_recv_entries[i].queue, &payload, &higherPriorityTaskWoken);
             }
         }
@@ -315,10 +368,10 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan) {
 }
 
 void CAN1_TX_IRQHandler(void) {
-    HAL_CAN_IRQHandler(&can1);
+    HAL_CAN_IRQHandler(hcan1);
 }
 
 void CAN1_RX0_IRQHandler(void) {
-    HAL_CAN_IRQHandler(&can1);
+    HAL_CAN_IRQHandler(hcan1);
 }
 
