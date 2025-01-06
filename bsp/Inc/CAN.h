ba51628f03fbb99df0bd94ce658fde5a3df224bc
Nathaniel Delgado <nathaniel.delgado@utexas.edu>
2025-01-06 12:46:49 -0600

    Added some notes

--- bsp/Inc/CAN.h
+++ bsp/Inc/CAN.h
@@ -4,6 +4,22 @@
 
 #include "stm32xx_hal.h"
 
+/* Notes for usage:
+ * CAN2 is usually a slave CAN, meaning that it shares
+ transistors with CAN1, meaning for CAN2 to work, CAN1
+ must be initialized. Thus, make sure CAN1 is initialized
+ before CAN2 is initialized.
+ * CAN2 usually shares the filter banks with CAN1, since it
+ is a slave CAN. Thus, consider the filter configs you pass
+ to the can_init function, when initializing CAN1 and CAN2.
+ * CAN3 is usually a master CAN, meaning you don't have to 
+ worry about sharing.
+ * This driver uses a macro to define which entries a CAN
+ interface should recieve on. Look at can1_recv_entries.h
+ for an example. Make sure these entries line up with the
+ filter config.
+ */
+
 // return code for can driver
 typedef enum {
     CAN_ERR,
