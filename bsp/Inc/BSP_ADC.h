4ec11829533c22ecc0a076b7f55493f7e84b74b0
Aarav Mahesh <aarav.mahesh@gmail.com>
2025-01-06 22:51:00 -0600

    final commit before pr

--- bsp/Inc/BSP_ADC.h
+++ bsp/Inc/BSP_ADC.h
@@ -1,21 +1,28 @@
-#ifndef BSP_ADC_H
-#define BSP_ADC_H
-
-#include "stm32xx.h"
+#include "stm32f4xx_hal.h"
 #include "queue.h"
+#include "FreeRTOS.h"
+#include "stm32xx_hal.h"
+#include "stm32f4xx_hal_init.c" // this can be put in "stm32xx_hal.h" later
+#include "semphr.h"
 
+/*
+NOTE ---
+For our boards we use the F413 which only has 1 ADC so no need for specific ADC config
+*/
+
+
+HAL_StatusTypeDef BSP_ADC_Init(ADC_InitTypeDef init, uint8_t bitNum, double Vcc, QueueHandle_t *rxQueue);
+/** 
+ * init             ADC_InitTypeDef 
+ * bitNum           uint8_t         number of bits in ADC
+ * Vcc              double
+ * rxQueue          QueueHandle_t   pointer to user-provided Queue handle
+*/
+
+
+HAL_StatusTypeDef BSP_ADC_OneShotRead(uint32_t channel, uint32_t samplingTime, bool blocking); 
 /**
- * @param peripheral:   ADC_HandleTypeDef containing unique indentifiers for ADC peripheral
- * @param clock:        Clock passed in to change sampling rate 
- * @param resolution:   ADC sensitivity to changes in voltage
- */
-HAL_StatusTypeDef BSP_ADC_Init(ADC_HandleTypeDef *periph);
-
-/**
- * @param periph:   ADC_HandleTypeDef containing unique indentifiers for ADC peripheral
- * @param q         Queue structure to read ADC conversion to
- * @return          Returns HAL_StatusTypeDef of peripheral (can check states, errors, etc.)
- */
-HAL_StatusTypeDef BSP_ADC_Read(ADC_HandleTypeDef *periph,  QueueHandle_t *RxQueue);
-
-#endif
+ *   channel        uint32_t        channel to read
+ *   samplingTime   uint32_t        ADC sampling time
+ *   blocking       bool            whether the process is blocking
+*/
--- bsp/Src/BSP_ADC.c
+++ bsp/Src/BSP_ADC.c
@@ -1,58 +1,163 @@
-#include "stm32xx.h"
-#include "queue.h" // use FreeRTOS queue
+#include "BSP_ADC.h"
 
-static QueueHandle_t myRxQueue;
-static ADC_HandleTypeDef myADC;
-static u_int32_t val; // ADC value
+ADC_HandleTypeDef hadc;
 
-/**
- * @param periph:   ADC_HandleTypeDef w/ channel, clock, and GPIO configs
- */
-HAL_StatusTypeDef BSP_ADC_Init(ADC_HandleTypeDef *periph) {
-    HAL_Init();
+double ADC_RAW_TO_VOLTAGE;  // conversion factor for readings
 
-    // store local variables of user pointers
-    myADC = periph;
+static SemaphoreHandle_t adc_send_semaphore = NULL;
+static StaticSemaphore_t adc_send_semaphore_buffer;
 
-    if (HAL_ADC_Init(&myADC) != HAL_OK) {
-        return HAL_ERROR;
+static QueueHandle_t* adcReadings;
+
+HAL_StatusTypeDef BSP_ADC_Init(ADC_InitTypeDef init, uint8_t bitNum, double Vcc, QueueHandle_t *rxQueue) {
+    
+    ADC_ChannelConfTypeDef sConfig = {0};
+
+    ADC_RAW_TO_VOLTAGE = 1.0 / Vcc * (float) bitNum; 
+
+    adcReadings = rxQueue;
+
+    adc_send_semaphore = xSemaphoreCreateBinaryStatic(&adc_send_semaphore_buffer);
+    xSemaphoreGive(adc_send_semaphore);
+
+    hadc.Init = init; // set the init structure to InitTypeDef
+    if (HAL_ADC_Init(&hadc) != HAL_OK) Error_Handler();
+}
+
+
+HAL_StatusTypeDef BSP_ADC_OneShotRead(uint32_t channel, uint32_t samplingTime, bool blocking) {
+
+    UBaseType_t space = uxQueueSpacesAvailable(adcReadings);    // check for empty space in the queue
+    if (space != 0) {
+        Error_Handler();
+
+        // could xQueueOverwrite last entry instead
     }
     
-    HAL_ADC_RegisterCallback(&myADC, HAL_ADC_CONVERSION_COMPLETE_CB_ID, HAL_ADC_ConvCpltCallback); // Register Complete Callback
-    HAL_ADC_RegisterCallback(&myADC, HAL_ADC_ERROR_CB_ID, HAL_ADC_ErrorCallback); // Register Error Callback
+    TickType_t timeout = blocking ? portMAX_DELAY : 0; // Set timeout to max delay if blocking, 0 if non-blocking
+
+    // take semphr
+    if (xSemaphoreTake(adc_send_semaphore, timeout) != pdTRUE) {
+        Error_Handler();
+    }
+
+    ADC_ChannelConfTypeDef sConfig = {0}; // resets the channel config
+
+    // configure only one channel
+    sConfig.Channel = channel;
+    sConfig.Rank = 1;
+    sConfig.SamplingTime = samplingTime;
+
+    if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK) {
+        Error_Handler();
+    }
+
+    // trigger interrupt to read 
+    HAL_ADC_Start_IT(&hadc);   
+}
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *h) {
+    /*
+    Interrupt triggers this callback when the conversion is complete
+    */
+   BaseType_t higherPriorityTaskWoken = pdFALSE;
+
+    if (h == &hadc) {
+        int rawVal = HAL_ADC_GetValue(&hadc);
+        double realVal = (double) rawVal * ADC_RAW_TO_VOLTAGE;
+
+        portENTER_Critical();
+
+        // push value to q
+        xQueueSendFromISR(adcReadings, &realVal, &higherPriorityTaskWoken);
+
+        // give semphr
+        xSemaphoreGive(adc_send_semaphore);
+
+        portEXIT_Critical();
+
+    } else {
+        Error_Handler();
+    }
+}
+
+void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc) {
+     _HAL_RCC_ADC1_CLK_ENABLE(); 
+
+    /**
+     * GPIO Inits for ADC Reading [using ADC1]
+     * PA_3     ADC1_3
+     * PC_0     ADC1_10
+     * PC_3     ADC1_13
+     * PA_7     ADC1_7
+     * PA_4     ADC1_4
+     * PB_1     ADC1_9
+     * PC_2     ADC1_12
+     * PA_0     ADC1_0
+     * PB_0     ADC1_8
+     * PA_5     ADC1_5
+     * PA_6     ADC1_6
+     * PA_7     ADC1_7
+     */
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_7|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6;
+
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;    
+    GPIO_InitStruct.Pull = GPIO_NOPULL;          
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    GPIO_InitStruct.Pin = GPIO_PIN_1;
     
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;    
+    GPIO_InitStruct.Pull = GPIO_NOPULL;          
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     
-    // Create NVIC Interrupt Handler and ISR stuff
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_0|GPIO_PIN_2;
+
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;    
+    GPIO_InitStruct.Pull = GPIO_NOPULL;          
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    __HAL_RCC_GPIOF_CLK_ENABLE();
+    GPIO_InitStruct.Pin = GPIO_PIN_4;
+
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;    
+    GPIO_InitStruct.Pull = GPIO_NOPULL;          
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    
+    HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+    HAL_NVIC_SetPriority(ADC_IRQn, 5, 0);
+    HAL_NVIC_EnableIRQ(ADC_IRQn);
 }
 
-/**
- * @param periph:   ADC_HandleTypeDef containing unique indentifiers for ADC peripheral
- * @param q         Queue structure to read ADC conversion to
- * @return          Returns HAL_StatusTypeDef of peripheral (can check states, errors, etc.)
- */
-HAL_StatusTypeDef BSP_ADC_Read(ADC_HandleTypeDef *periph,  QueueHandle_t *RxQueue) {
-    myRxQueue = RxQueue;
 
-    // Check if Queue is full
+void HAL_ADC_MspDeInit(ADC_HandleTypeDef *hadc) {
+    __HAL_RCC_ADC1_CLK_DISABLE();
 
+    // gpio deinits
+    HAL_GPIO_DeInit(GPIOA, GPIO_PIN_0|GPIO_PIN_7|GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6);
+    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_1);
+    HAL_GPIO_DeInit(GPIOC, GPIO_PIN_3|GPIO_PIN_0|GPIO_PIN_2);
+    HAL_GPIO_DeInit(GPIOF, GPIO_PIN_4);
 
-    // Do ADC Conversion
-    HAL_ADC_Start_IT(&myADC);
-
-    // Store ADC Conversion in Queue
-    xQueueSend(myRxQueue, &val, 100); 
-
+    HAL_NVIC_DisableIRQ(ADC_IRQn);
 }
 
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *periph) {
-    // Callback when ADC conversion is finished
-    val = HAL_ADC_GetValue(&periph);
 
+void Error_Handler(void)
+{
+    // should add comm codes in next PR
+  __disable_irq();
+  while (1)
+  {
+  }
 }
-
-void HAL_ADC_ErrorCallback(ADC_HandleTypeDef *periph) {
-    // Callback when ADC conversion results in error
-
-}
-
