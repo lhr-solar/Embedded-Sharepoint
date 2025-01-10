9f68fea8b4a7d25fe40a9bde7665f737eb9f11cf
Aneri Patel <anerip@AneriLaptop2.>
2025-01-10 16:47:11 -0600

    updated test

--- Test/Tests/SPI_Test.c
+++ Test/Tests/SPI_Test.c
@@ -1,83 +1,98 @@
-//#include <stdint.h>
-#include "stdbool.h"
-#include "stm32x4xx_hal_init.h"
-#include "stm32xx_hal.h"
-#include "stm32xx_hal_spi.h"
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_spi.h"
 
 SPI_HandleTypeDef hspi1;
 
-/**
- * @brief SPI1 Initialization Function
- * @param None
- * @retval None
- */
-static void MX_SPI1_Init(void) {
-  hspi1.Instance = SPI1;
-  hspi1.Init.Mode = SPI_MODE_MASTER;
-  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
-  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
-  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
-  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
-  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
-  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  hspi1.Init.CRCPolynomial = 0; //dont care because crccalc is disabled
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  __SPI1_CLK_ENABLE();
-}
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
- /**  if (HAL_SPI_Init(&hspi1) != HAL_OK) {
-    // Error_Handler();
-  }
-} 
-*/
-
-/**
- * @brief GPIO Initialization Function
- * @param None
- * @retval None
- */
-static void MX_GPIO_Init(void) {
-  GPIO_InitTypeDef SPI1_conf1 = {.Pin = GPIO_PIN_5, // SCK
-                                   .Mode = GPIO_MODE_AF_PP, //might be AF
-                                   .Pull = GPIO_NOPULL,
-                                   .Speed = GPIO_SPEED_FREQ_LOW,
-                                   .Alternate = GPIO_AF5_SPI1};
-  GPIO_InitTypeDef SPI1_conf2 = {.Pin = GPIO_PIN_6, // MISO
-                                   .Mode = GPIO_MODE_AF_PP,
-                                   .Pull = GPIO_NOPULL,
-                                   .Speed = GPIO_SPEED_FREQ_LOW,
-                                   .Alternate = GPIO_AF5_SPI1};
-  GPIO_InitTypeDef SPI1_conf3 = {.Pin = GPIO_PIN_7, // MOSI
-                                   .Mode = GPIO_MODE_AF_PP,
-                                   .Pull = GPIO_NOPULL,
-                                   .Speed = GPIO_SPEED_FREQ_LOW,
-                                   .Alternate = GPIO_AF5_SPI1};
-
-  /* GPIO Ports Clock Enable */
-  __GPIOA_CLK_ENABLE();
-
-  HAL_GPIO_Init(GPIOA, &SPI1_conf1);
-  HAL_GPIO_Init(GPIOA, &SPI1_conf2);
-  HAL_GPIO_Init(GPIOA, &SPI1_conf3);
-}
-
-int main(void) {
-  HAL_Init();
-  // SystemClock_Config();
-  MX_GPIO_Init();
-  MX_SPI1_Init();
-
-  uint8_t txData[3] = {1,2,3}; // Example data to transmit
-
-  while (1) {
-    // Example: Transmit data over SPI
-    if (HAL_SPI_GetState(&hspi1) == HAL_SPI_STATE_READY) {
-      HAL_SPI_Transmit(&hspi1, txData, sizeof(txData), HAL_MAX_DELAY);
-
-      HAL_Delay(100); // Delay between transmissions
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+    RCC_OscInitStruct.PLL.PLLM = 16;
+    RCC_OscInitStruct.PLL.PLLN = 336;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+    RCC_OscInitStruct.PLL.PLLQ = 7;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
     }
-  }
+
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                                |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+static void MX_SPI1_Init(void) {
+    hspi1.Instance = SPI1;
+    hspi1.Init.Mode = SPI_MODE_MASTER;
+    hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+    hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+    hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+    hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+    hspi1.Init.NSS = SPI_NSS_SOFT;
+    hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+    hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+    hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+    hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+    hspi1.Init.CRCPolynomial = 0;
+
+    __HAL_RCC_SPI1_CLK_ENABLE();
+
+    if (HAL_SPI_Init(&hspi1) != HAL_OK) {
+        Error_Handler();
+    }
+}
+
+static void MX_GPIO_Init(void) {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    
+    GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+}
+
+
+void Error_Handler(void)
+{
+    __disable_irq();
+    while (1)
+    {
+    }
+}
+
+int main(void)
+{
+    HAL_Init();
+    SystemClock_Config();  // This is necessary for proper operation
+    MX_GPIO_Init();
+    MX_SPI1_Init();
+
+    uint8_t txData[3] = {1,2,3};
+
+while (1)
+{
+    HAL_SPI_Transmit(&hspi1, txData, sizeof(txData), HAL_MAX_DELAY);
+    HAL_Delay(1000);
+}
 }
