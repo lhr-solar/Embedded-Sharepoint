549848423dccab53bf12679c408179aaf415780b
Shaun Kurian <shaunkurian345@gmail.com>
2025-01-18 12:10:17 -0600

    ifdef i2c1 guard for i2c1 test

--- test/tests/BasicI2C_IT.c
+++ test/tests/BasicI2C_IT.c
@@ -3,6 +3,7 @@
 #include "task.h"
 #include "BSP_I2C.h"
 
+#ifdef I2C1
 /* Private function prototypes -----------------------------------------------*/
 void Clock_Config(void);
 static void MX_GPIO_Init(void);
@@ -184,3 +185,4 @@ void assert_failed(uint8_t *file, uint32_t line) {
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
+#endif
