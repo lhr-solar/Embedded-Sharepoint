3a2368c684e65b16ce9e2eabc7d57614f5dda265
fangjim <jfang875@gmail.com>
2025-02-08 12:53:06 -0600

    tweak test script

--- test/makeAllTests.sh
+++ test/makeAllTests.sh
@@ -5,38 +5,31 @@ port_list=("stm32f401re" "stm32f413rht" "stm32f429zit" "stm32f446ret" "stm32l431
 # Boards that should run UART tests since they support UART4 & UART5
 uart_enabled_ports=("stm32f446ret" "stm32l476rgt")
 
-
 for port in "${port_list[@]}"; do
-    
     make clean
     for tests_dir in tests/*; do
         test_name="${tests_dir#tests/}"
         test_name="${test_name%.c}"
         
-        # Skip the "can" test for stm32f401*e or stm32f401*c
+        # Skip CAN tests for f401
         if [[ "$port" =~ ^stm32f401.*[ec]$ ]] && ([[ "$test_name" == "can" ]] || [[ "$test_name" == "can_mt" ]]); then
             echo "Skipping the CAN test for $port because it does not support CAN"
             continue
         fi
-
-        # For UART tests, only run on specified boards
-        if ([[ "$test_name" == "uart" ]] || [[ "$test_name" == "uart_mt" ]]); then
-            # if the port is not in the list of boards that support UART, skip it
-            if [[ ! " ${uart_enabled_ports[@]} " =~ " ${port} " ]]; then
-                echo "Skipping the UART test for $port because it does not support UART"
-                continue
-            fi
+        
+        # Skip UART tests for non-UART boards
+        if ([[ "$test_name" == "uart" ]] || [[ "$test_name" == "uart_mt" ]]) && [[ ! " ${uart_enabled_ports[@]} " =~ " ${port} " ]]; then
+            echo "Skipping the UART test for $port because it does not support UART4 & UART5"
+            continue
         fi
 
-
         echo "Compiling the test - $test_name for $port"
-
+        
         if ! make TEST="$test_name" PROJECT_TARGET="$port" -j; then
             echo "Errors occurred while compiling $test_name.c using $port"
             exit 1
         fi
     done
-
 done
 
 echo "Jolly Good! All tests compiled successfully"
