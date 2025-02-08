7266b90e9d8d216e42747a06461840bd5b11b3f7
Lakshay983 <lakshay45gupta@gmail.com>
2025-02-08 15:28:04 -0600

    poop

--- scripts/makeAllTests.sh
+++ scripts/makeAllTests.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-
-
 port_list=("stm32f401re" "stm32f413rht" "stm32f429zit" "stm32f446ret" "stm32l431cbt" "stm32l476rgt")
+
+makefile_dir="Embedded-Sharepoint/test/Makefile"
+tests_dir="Embedded-Sharepoint/test/tests"
 
 for port in "${port_list[@]}"; do
     
-    make clean
-    for tests_dir in tests/*; do
-        test_name="${tests_dir#tests/}"
+    make -f "$makefile_dir" clean
+    for test_file in "$tests_dir"/*; do
+        test_name="${test_file#$tests_dir/}"
         test_name="${test_name%.c}"
         
         # Skip the "can" test for stm32f401*e or stm32f401*c
@@ -19,7 +20,7 @@ for port in "${port_list[@]}"; do
 
         echo "Compiling the test - $test_name for $port"
 
-        if ! make TEST="$test_name" PROJECT_TARGET="$port" -j; then
+        if ! make -f "$makefile_dir" TEST="$test_name" PROJECT_TARGET="$port" -j; then
             echo "Errors occurred while compiling $test_name.c using $port"
             exit 1
         fi
