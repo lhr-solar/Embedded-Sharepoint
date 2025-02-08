4c07b1c8cc344cb2f14c54852f70856c6060d4c4
fangjim <jfang875@gmail.com>
2025-02-08 10:30:33 -0600

    altered init in tests for f446ret

--- test/tests/uart.c
+++ test/tests/uart.c
@@ -88,8 +88,11 @@ static void MX_UART4_Init(void)
     huart4->Init.Mode = UART_MODE_TX_RX;
     huart4->Init.HwFlowCtl = UART_HWCONTROL_NONE;
     huart4->Init.OverSampling = UART_OVERSAMPLING_16;
+    
+    #ifdef STM32L4xx
     huart4->Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
     huart4->AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+    #endif /* STM32L4xx */
     
     if (HAL_UART_Init(huart4) != HAL_OK) {
         Error_Handler();
