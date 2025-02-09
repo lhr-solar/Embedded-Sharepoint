38005c00f53d813f1def3bef83a77456da0f315d
Lakshay983 <lakshay45gupta@gmail.com>
2025-02-09 14:28:04 -0600

    add compose down comment

--- scripts/dockerInitialize.sh
+++ scripts/dockerInitialize.sh
@@ -10,5 +10,5 @@ echo "REMOTE_USER=$USER" >> $ENV_FILE
 echo "REMOTE_UID=$(id -u)" >> $ENV_FILE
 echo "REMOTE_GID=$(id -g)" >> $ENV_FILE
 
-# Bring down the docker-compose setup
+# Need to bring down compose to prevent using other projects compose files
 docker compose -f $DOCKER_COMPOSE_FILE down
