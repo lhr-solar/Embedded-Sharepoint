99edae211117626fc2005a8c75f91a8338e85c17
Aarav Mahesh <aarav.mahesh@gmail.com>
2025-02-24 14:40:07 -0600

    commit unresolved changes

--- common/Inc/stm32xx_hal.h
+++ common/Inc/stm32xx_hal.h
@@ -1,23 +1,27 @@
+#ifndef STM32xx_HAL_H
+#define STM32xx_HAL_H
+
+// STM32
 #if defined(STM32F4xx)
     #include "stm32f4xx.h"
     #include "stm32f4xx_hal.h"
-    #include "stm32f4xx_hal_rcc.h"
-
-    // Supported ports
-    #if defined(STM32F401xE)
-        #include "stm32f401xe.h"
-    #elif defined(STM32F413xx)
-        #include "stm32f413xx.h"
-    #elif defined(STM32F446xx)
-        #include "stm32f446xx.h"
-    #endif
-
 #elif defined(STM32L4xx)
     #include "stm32l4xx.h"
     #include "stm32l4xx_hal.h"
-    #include "stm32l4xx_hal_rcc.h"
 #else
     #error "No valid STM32 series defined. Please define either STM32F4xx or STM32L4xx."
 #endif
-
 #include "stm32x4xx_hal_init.h"
+
+// FreeRTOS
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "timers.h"
+#include "semphr.h"
+
+// Standard Library
+#include <stdint.h>
+#include <stdbool.h>
+
+#endif /* STM32xs_HAL_H */
