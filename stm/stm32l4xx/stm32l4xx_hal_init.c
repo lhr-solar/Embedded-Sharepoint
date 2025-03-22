3b1c29c0f2d9753fc70d5a0f72f040def304a3d0
IshDeshpa <ishdeshpa@gmail.com>
2025-03-22 20:44:44 +0000

    tryna fix clock config

--- stm/stm32l4xx/stm32l4xx_hal_init.c
+++ stm/stm32l4xx/stm32l4xx_hal_init.c
@@ -52,7 +52,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
   RCC_OscInitStruct.PLL.PLLM = 1;
-  RCC_OscInitStruct.PLL.PLLN = 40;
+  RCC_OscInitStruct.PLL.PLLN = 20;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
   RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
   RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
