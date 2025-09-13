7e9c1d09156998f0f51709a2f5eda5a7e0a4c5f5
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2025-09-13 15:14:37 -0500

    blonky

--- test/tests/blinky.c
+++ test/tests/blinky.c
@@ -1,20 +1,42 @@
 #include "stm32xx_hal.h"
 
+#ifdef stm32l432kcu
+    #define LED_PIN GPIO_PIN_3
+    #define LED_PORT GPIOB
+#else
+    #define LED_PIN GPIO_PIN_5
+    #define LED_PORT GPIOA
+#endif
+
+void Heartbeat_Clock_Init() {
+    switch ((uint32_t)LED_PORT) {
+        case (uint32_t)GPIOA:
+            __HAL_RCC_GPIOA_CLK_ENABLE();
+            break;
+        case (uint32_t)GPIOB:
+            __HAL_RCC_GPIOB_CLK_ENABLE();
+            break;
+        case (uint32_t)GPIOC:
+            __HAL_RCC_GPIOC_CLK_ENABLE();
+            break;
+    }
+}
+
 int main(){
     HAL_Init();
 
     GPIO_InitTypeDef led_config = {
         .Mode = GPIO_MODE_OUTPUT_PP,
         .Pull = GPIO_NOPULL,
-        .Pin = GPIO_PIN_5
+        .Pin = LED_PIN
     };
     
-    __HAL_RCC_GPIOA_CLK_ENABLE(); // enable clock for GPIOA
-    HAL_GPIO_Init(GPIOA, &led_config); // initialize GPIOA with led_config
+    Heartbeat_Clock_Init(); // enable clock for GPIOA
+    HAL_GPIO_Init(LED_PORT, &led_config); // initialize GPIOA with led_config
 
     while(1){
-        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
-        HAL_Delay(1000);
+        HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
+        HAL_Delay(500);
     }
 
     return 0;
