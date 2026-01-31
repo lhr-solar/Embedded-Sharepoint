8410915646130c3ad3bf58a928c2dc2c942b445c
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2026-01-31 10:45:32 -0600

    remove can_status enum from CAN.h

--- bsp/Inc/CAN.h
+++ bsp/Inc/CAN.h
@@ -89,14 +89,6 @@
  filter config.
  */
 
-// return code for can driver
-typedef enum {
-    CAN_ERR,   // unsuccessful operation
-    CAN_OK,
-    CAN_SENT,  // successful send
-    CAN_RECV,  // successful recieve
-    CAN_EMPTY, // recieved nothing with no errors
-} can_status_t;
 
 // can handlers
 #ifdef CAN1
