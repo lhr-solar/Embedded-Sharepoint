5a078628ed5f7595c800172ef95368997eb6d948
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2026-02-13 16:30:15 -0600

    enable idle hook

--- common/Inc/FreeRTOSConfig.h
+++ common/Inc/FreeRTOSConfig.h
@@ -54,7 +54,7 @@
 #define configUSE_PREEMPTION                     1
 #define configSUPPORT_STATIC_ALLOCATION          1
 #define configSUPPORT_DYNAMIC_ALLOCATION         0
-#define configUSE_IDLE_HOOK                      0
+#define configUSE_IDLE_HOOK                      1
 #define configUSE_TICK_HOOK                      0
 #define configCPU_CLOCK_HZ                       ( SystemCoreClock )
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
@@ -71,10 +71,6 @@
 /* Added by us */
 #define configUSE_QUEUE_SETS                     1
 
-// Only use the idle hook if user enables it
-#ifndef configUSE_IDLE_HOOK
-  #define configUSE_IDLE_HOOK                    0  
-#endif /*configUSE_IDLE_HOOK*/
 
 
 /* Defaults to size_t for backward compatibility, but can be changed
