15c3c495b2de63241faea8155129ee6d52439000
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2026-02-28 12:27:31 -0600

    >:(

--- test/tests/blinky_test.c
+++ test/tests/blinky_test.c
@@ -1,10 +1,10 @@
 #include "stm32xx_hal.h"
 
 // Heartbeat pin on stm32l432kcu is PB3
-#ifdef STM32L432xx
+#ifdef defined(STM32L432xx)
     #define LED_PIN GPIO_PIN_3
     #define LED_PORT GPIOB
-#elif STM32L431xx
+#elif defined(STM32L431xx)
     #define LED_PIN GPIO_PIN_11
     #define LED_PORT GPIOB
 #elif defined(STM32G473xx)
