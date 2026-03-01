e968cadb30db04e250247e26e21613efc4d83d62
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2026-03-01 11:31:20 -0600

    balls

--- driver/Inc/emaFilter.h
+++ driver/Inc/emaFilter.h
@@ -2,13 +2,30 @@
 
 #include <stdint.h>
 
+typedef enum
+{
+    EMA_U16,
+    EMA_I16,
+    EMA_U32,
+    EMA_I32
+} ema_type_t;
+
 typedef struct
 {
-    int32_t y;       // Filter state (scaled)
-    uint16_t k;      // Alpha numerator
-    uint8_t shift;   // Alpha denominator = 2^shift
+    ema_type_t type;
+    uint16_t k;
+    uint8_t shift;
+
+    union
+    {
+        uint16_t u16;
+        int16_t  i16;
+        uint32_t u32;
+        int32_t  i32;
+    } y;
+
 } ema_filter_t;
 
-void ema_init(ema_filter_t *filt, uint16_t k, uint8_t shift, int32_t initial_value);
+void ema_init(ema_filter_t *f, ema_type_t type, uint16_t k, uint8_t shift, int32_t initial);
+void ema_update(ema_filter_t *f, int32_t input);
 
-int32_t ema_update(ema_filter_t *filt, int32_t input);
--- driver/Src/emaFilter.c
+++ driver/Src/emaFilter.c
@@ -1,15 +1,50 @@
-#include "fixed_ema.h"
+#include "emaFilter.h"
 
-void ema_init(ema_filter_t *filt, uint16_t k, uint8_t shift, int32_t initial_value)
+void ema_init(ema_filter_t *f, ema_type_t type, uint16_t k, uint8_t shift, int32_t initial)
 {
-    filt->k = k;
-    filt->shift = shift;
-    filt->y = initial_value;
+    f->type = type;
+    f->k = k;
+    f->shift = shift;
+
+    switch(type)
+    {
+        case EMA_U16: f->y.u16 = (uint16_t)initial; break;
+        case EMA_I16: f->y.i16 = (int16_t)initial;  break;
+        case EMA_U32: f->y.u32 = (uint32_t)initial; break;
+        case EMA_I32: f->y.i32 = (int32_t)initial;  break;
+    }
 }
 
-int32_t ema_update(ema_filter_t *filt, int32_t input)
+void ema_update(ema_filter_t *f, int32_t input)
 {
-    int32_t error = input - filt->y;
-    filt->y += (error * filt->k) >> filt->shift;
-    return filt->y;
+    int64_t error;
+    int64_t delta;
+
+    switch(f->type)
+    {
+        case EMA_U16:
+            error = (int64_t)(uint16_t)input - (int64_t)f->y.u16;
+            delta = (error * f->k) >> f->shift;
+            f->y.u16 += (uint16_t)delta;
+            break;
+
+        case EMA_I16:
+            error = (int64_t)(int16_t)input - (int64_t)f->y.i16;
+            delta = (error * f->k) >> f->shift;
+            f->y.i16 += (int16_t)delta;
+            break;
+
+        case EMA_U32:
+            error = (int64_t)(uint32_t)input - (int64_t)f->y.u32;
+            delta = (error * f->k) >> f->shift;
+            f->y.u32 += (uint32_t)delta;
+            break;
+
+        case EMA_I32:
+        default:
+            error = (int64_t)(int32_t)input - (int64_t)f->y.i32;
+            delta = (error * f->k) >> f->shift;
+            f->y.i32 += (int32_t)delta;
+            break;
+    }
 }
