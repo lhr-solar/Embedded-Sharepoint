8abd9a8e8a6e94a66e92565bdba44f4fa50e06ba
Michael Adeluyi <139656581+micade145@users.noreply.github.com>
2026-03-07 21:40:01 -0600

    tickless idle macro in FreeRTOSConfig.h (#226)

--- common/Inc/FreeRTOSConfig.h
+++ common/Inc/FreeRTOSConfig.h
@@ -55,6 +55,7 @@
 #define configSUPPORT_DYNAMIC_ALLOCATION         0
 #define configUSE_IDLE_HOOK                      1
 #define configUSE_TICK_HOOK                      0
+#define configUSE_TICKLESS_IDLE                  1
 #define configCPU_CLOCK_HZ                       ( SystemCoreClock )
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 7 )
