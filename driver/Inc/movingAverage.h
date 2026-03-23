94b7ae5cfa40480a20b9581e4c00d4b6e6472374
Lakshay983 <56173382+Lakshay983@users.noreply.github.com>
2026-03-23 16:41:47 -0500

    balls

--- driver/Inc/movingAverage.h
+++ driver/Inc/movingAverage.h
@@ -3,36 +3,38 @@
 #include <stdint.h>
 
 #ifndef MOVING_AVERAGE_WINDOW_SIZE
-#define MOVING_AVERAGE_WINDOW_SIZE
-#endif /*MOVING_AVERAGE_WINDOW_SIZE*/
-
+#define MOVING_AVERAGE_WINDOW_SIZE 8
+#endif
 
 #define DEFINE_MOVING_AVG(NAME, TYPE, SUM_TYPE)             \
 typedef struct {                                            \
-    TYPE buffer[WINDOW_SIZE];                               \
+    TYPE buffer[MOVING_AVERAGE_WINDOW_SIZE];                \
     SUM_TYPE sum;                                           \
     uint8_t index;                                          \
     uint8_t count;                                          \
 } NAME;                                                     \
                                                             \
-void NAME##_init(NAME *ma) {                                \
+static inline void NAME##_init(NAME *ma) {                  \
     ma->sum = 0;                                            \
     ma->index = 0;                                          \
     ma->count = 0;                                          \
-    for (int i = 0; i < WINDOW_SIZE; i++) {                 \
+    for (uint8_t i = 0; i < MOVING_AVERAGE_WINDOW_SIZE; i++) { \
         ma->buffer[i] = 0;                                  \
     }                                                       \
 }                                                           \
                                                             \
-TYPE NAME##_update(NAME *ma, TYPE sample) {                 \
+static inline TYPE NAME##_update(NAME *ma, TYPE sample) {   \
     ma->sum -= ma->buffer[ma->index];                       \
     ma->buffer[ma->index] = sample;                         \
     ma->sum += sample;                                      \
-    ma->index = (ma->index + 1) % WINDOW_SIZE;              \
+    ma->index = (ma->index + 1) % MOVING_AVERAGE_WINDOW_SIZE; \
                                                             \
-    if (ma->count < WINDOW_SIZE) {                          \
+    if (ma->count < MOVING_AVERAGE_WINDOW_SIZE) {           \
         ma->count++;                                        \
     }                                                       \
                                                             \
+    if(ma->count == 0){                                     \
+        return 0;                                           \
+    }                                                       \
     return (TYPE)(ma->sum / ma->count);                     \
 }
