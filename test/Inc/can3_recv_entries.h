199652bcf58d86e952ff2f79cf407a6842c99a11
Shuopu Wang <shuopuwang027@gmail.com>
2026-03-26 00:54:58 -0500

    fixed merge conflicts

--- test/Inc/can3_recv_entries.h
+++ test/Inc/can3_recv_entries.h
@@ -4,9 +4,5 @@
 CAN_RECV_ENTRY(0x001, 5, false)
 CAN_RECV_ENTRY(0x002, 8, false)
 CAN_RECV_ENTRY(0x003, 3, false)
-<<<<<<< HEAD
-CAN_RECV_ENTRY(0x321, 4, true)
-=======
 CAN_RECV_ENTRY(0x004, 4, true)
 CAN_RECV_ENTRY(BPS_FAULT_ID, 4, true)
->>>>>>> main
